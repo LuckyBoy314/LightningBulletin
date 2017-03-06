c2bd667b4fa25d8d43fa171d8c20b2757c6cfcc6
LuckyBoy314 <zhufeng314@163.comm>
2017-03-06 23:26:29 +0800

    更新UI

--- UI.py
+++ UI.py
@@ -9,16 +9,50 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import time
+from multiprocessing import Process
+from shutil import move,copy
+from Main import mainProcess
+import  os
+
+class WorkProcess(QtCore.QObject):
+    '''
+    自定义进程，进程由Python模块multiprocess创建
+    并由pyqt的信号处理机制创建与主调进程的通信，通知子进程是否结束
+    '''
+    trigger = QtCore.pyqtSignal()
+
+    def __init__(self):
+        super(WorkProcess, self).__init__()
+        self.timer = QtCore.QTimer(self)  #创建计时器
+        self.timer.timeout.connect(self.timeOut)  #连接计时器周期结束事件
+
+    def beginRun(self, in_parameters):
+        self.p = Process(target= mainProcess, kwargs=in_parameters) #设置子进程调用程序和参数
+        self.p.start()         #启动子进程
+        self.timer.start(1000) #启动计时器，计时器周期为1秒（1000毫秒）
+
+    def timeOut(self):  #计时器一个周期结束即触发一个timeout事件，然后调用此函数，主要用于检测子进程是否结束
+        if not self.p.is_alive():  # 当前子进程完成后
+            self.trigger.emit()  # 发出处理完成信息
+            self.timer.stop()  # 并停止计时器，否则在同一进程下再次运行会导致过早判断子进程的状态
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+
         self.lastyear = int(time.strftime('%Y', time.localtime(time.time()))) - 1
+        self.in_parameters = {u'datetime': str(self.lastyear) + u'年',
+                              u'province':u'河南',
+                              u'target_area': u'新乡市',
+                              u'density_cell': u'10',
+                              u'density_class': 10,
+                              u'day_cell': u'15',
+                              u'day_class': 10,
+                              u'out_type': u'tiff'}
         self.setupUi()
 
     def setupUi(self):
-        self.setObjectName("self")
-        self.resize(925, 935)
+        self.setObjectName("MainWindow")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("resource/weather-thunder.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setWindowIcon(icon)
@@ -293,20 +327,20 @@ class MainWindow(QtWidgets.QMainWindow):
         self.retranslateUi()
         QtCore.QMetaObject.connectSlotsByName(self)
 
-        self.center()
+        self.initSizePosition()
         self.show()
 
-        self.handle_events()
+        self.handleEvents()
 
     def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        self.setWindowTitle(_translate("self", "雷电公报"))
-        self.year_label.setText(_translate("self", "年份"))
-        self.year_DateEdit.setDisplayFormat(_translate("self", "yyyy"))
-        self.province_label.setText(_translate("self", "省份"))
-        self.province_comboBox.setItemText(0, _translate("self", "河南"))
-        self.province_comboBox.setItemText(1, _translate("self", "浙江"))
-        self.target_area_label.setText(_translate("self", "目标区域"))
+        self.setWindowTitle(_translate("MainWindow", "雷电公报"))
+        self.year_label.setText(_translate("MainWindow", "年份"))
+        self.year_DateEdit.setDisplayFormat(_translate("MainWindow", "yyyy"))
+        self.province_label.setText(_translate("MainWindow", "省份"))
+        self.province_comboBox.setItemText(0, _translate("MainWindow", "河南"))
+        self.province_comboBox.setItemText(1, _translate("MainWindow", "浙江"))
+        self.target_area_label.setText(_translate("MainWindow", "目标区域"))
         self.target_area_comboBox.setItemText(0, _translate("MainWindow", "新乡市"))
         self.target_area_comboBox.setItemText(1, _translate("MainWindow", "延津县"))
         self.target_area_comboBox.setItemText(2, _translate("MainWindow", "新乡县"))
@@ -314,76 +348,241 @@ class MainWindow(QtWidgets.QMainWindow):
         self.target_area_comboBox.setItemText(4, _translate("MainWindow", "卫辉市"))
         self.target_area_comboBox.setItemText(5, _translate("MainWindow", "获嘉县"))
         self.target_area_comboBox.setItemText(6, _translate("MainWindow", "封丘县"))
-        self.density_cell_label.setText(_translate("self", "插值网格大小"))
-        self.density_class_label.setText(_translate("self", "制图分类数目"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.density_tab), _translate("self", "闪电密度分布图"))
-        self.day_cell_label.setText(_translate("self", "插值半径大小"))
-        self.day_class_label.setText(_translate("self", "制图分类数目"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.day_tab), _translate("self", "雷暴日分布图"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.regions_stats_tab), _translate("self", "分区统计"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.month_stats_tab), _translate("self", "分月统计"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.hour_stats_tab), _translate("self", "分时段统计"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.intensity_stats_tab), _translate("self", "强度统计"))
-        self.file_menu.setTitle(_translate("self", "文件"))
-        self.help_menu.setTitle(_translate("self", "帮助"))
-        self.action_menu.setTitle(_translate("self", "操作"))
-        self.toolBar.setWindowTitle(_translate("self", "toolBar"))
-        self.help_action.setText(_translate("self", "使用说明"))
-        self.help_action.setStatusTip(_translate("self", "使用说明"))
-        self.help_action.setShortcut(_translate("self", "F1"))
-        self.about_action.setText(_translate("self", "关于"))
-        self.about_action.setStatusTip(_translate("self", "关于"))
-        self.about_action.setShortcut(_translate("self", "F2"))
-        self.export_charts_action.setText(_translate("self", "导出图表"))
-        self.export_charts_action.setToolTip(_translate("self", "导出所有GIS图和统计图表"))
-        self.export_charts_action.setStatusTip(_translate("self", "导出所有GIS图和统计图表"))
-        self.export_charts_action.setShortcut(_translate("self", "Alt+C"))
-        self.export_doc_action.setText(_translate("self", "导出文档"))
-        self.export_doc_action.setToolTip(_translate("self", "导出公报word文档"))
-        self.export_doc_action.setStatusTip(_translate("self", "导出公报word文档"))
-        self.export_doc_action.setShortcut(_translate("self", "Alt+D"))
-        self.load_data_action.setText(_translate("self", "加载数据"))
-        self.load_data_action.setToolTip(_translate("self", "制作雷电公报图表前，必须加载电闪数据"))
-        self.load_data_action.setStatusTip(_translate("self", "制作雷电公报图表前，必须加载电闪数据"))
-        self.load_data_action.setShortcut(_translate("self", "Ctrl+N"))
-        self.execute_action.setText(_translate("self", "执行"))
-        self.execute_action.setToolTip(_translate("self", "点击开始执行"))
-        self.execute_action.setStatusTip(_translate("self", "点击开始执行"))
-        self.execute_action.setShortcut(_translate("self", "F5"))
-        self.open_doc_action.setText(_translate("self", "打开文档"))
-        self.open_doc_action.setToolTip(_translate("self", "打开公报word文档"))
-        self.open_doc_action.setStatusTip(_translate("self", "打开公报word文档"))
-        self.open_doc_action.setShortcut(_translate("self", "Ctrl+D"))
-        self.open_charts_action.setText(_translate("self", "打开图表"))
-        self.open_charts_action.setToolTip(_translate("self", "打开公报统计图表"))
-        self.open_charts_action.setStatusTip(_translate("self", "打开公报统计图表"))
-        self.open_charts_action.setShortcut(_translate("self", "Ctrl+C"))
-        self.exit_action.setText(_translate("self", "退出"))
-        self.exit_action.setShortcut(_translate("self", "F4"))
+        self.density_cell_label.setText(_translate("MainWindow", "插值网格大小"))
+        self.density_class_label.setText(_translate("MainWindow", "制图分类数目"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.density_tab), _translate("MainWindow", "闪电密度分布图"))
+        self.day_cell_label.setText(_translate("MainWindow", "插值半径大小"))
+        self.day_class_label.setText(_translate("MainWindow", "制图分类数目"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.day_tab), _translate("MainWindow", "雷暴日分布图"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.regions_stats_tab), _translate("MainWindow", "分区统计"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.month_stats_tab), _translate("MainWindow", "分月统计"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.hour_stats_tab), _translate("MainWindow", "分时段统计"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.intensity_stats_tab), _translate("MainWindow", "强度统计"))
+        self.file_menu.setTitle(_translate("MainWindow", "文件"))
+        self.help_menu.setTitle(_translate("MainWindow", "帮助"))
+        self.action_menu.setTitle(_translate("MainWindow", "操作"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.help_action.setText(_translate("MainWindow", "使用说明"))
+        self.help_action.setStatusTip(_translate("MainWindow", "使用说明"))
+        self.help_action.setShortcut(_translate("MainWindow", "F1"))
+        self.about_action.setText(_translate("MainWindow", "关于"))
+        self.about_action.setStatusTip(_translate("MainWindow", "关于"))
+        self.about_action.setShortcut(_translate("MainWindow", "F2"))
+        self.export_charts_action.setText(_translate("MainWindow", "导出图表"))
+        self.export_charts_action.setToolTip(_translate("MainWindow", "导出所有GIS图和统计图表"))
+        self.export_charts_action.setStatusTip(_translate("MainWindow", "导出所有GIS图和统计图表"))
+        self.export_charts_action.setShortcut(_translate("MainWindow", "Alt+C"))
+        self.export_doc_action.setText(_translate("MainWindow", "导出文档"))
+        self.export_doc_action.setToolTip(_translate("MainWindow", "导出公报word文档"))
+        self.export_doc_action.setStatusTip(_translate("MainWindow", "导出公报word文档"))
+        self.export_doc_action.setShortcut(_translate("MainWindow", "Alt+D"))
+        self.load_data_action.setText(_translate("MainWindow", "加载数据"))
+        self.load_data_action.setToolTip(_translate("MainWindow", "制作雷电公报图表前，必须加载电闪数据"))
+        self.load_data_action.setStatusTip(_translate("MainWindow", "制作雷电公报图表前，必须加载电闪数据"))
+        self.load_data_action.setShortcut(_translate("MainWindow", "Ctrl+N"))
+        self.execute_action.setText(_translate("MainWindow", "执行"))
+        self.execute_action.setToolTip(_translate("MainWindow", "点击开始执行"))
+        self.execute_action.setStatusTip(_translate("MainWindow", "点击开始执行"))
+        self.execute_action.setShortcut(_translate("MainWindow", "F5"))
+        self.open_doc_action.setText(_translate("MainWindow", "打开文档"))
+        self.open_doc_action.setToolTip(_translate("MainWindow", "打开公报word文档"))
+        self.open_doc_action.setStatusTip(_translate("MainWindow", "打开公报word文档"))
+        self.open_doc_action.setShortcut(_translate("MainWindow", "Ctrl+D"))
+        self.open_charts_action.setText(_translate("MainWindow", "打开图表"))
+        self.open_charts_action.setToolTip(_translate("MainWindow", "打开公报统计图表"))
+        self.open_charts_action.setStatusTip(_translate("MainWindow", "打开公报统计图表"))
+        self.open_charts_action.setShortcut(_translate("MainWindow", "Ctrl+C"))
+        self.exit_action.setText(_translate("MainWindow", "退出"))
+        self.exit_action.setShortcut(_translate("MainWindow", "F4"))
 
-    def handle_events(self):
-        self.about_action.triggered.connect(self.show_about)
-        self.load_data_action.triggered.connect(self.load_data)
-
-    def center(self):
+    def initSizePosition(self):
+        # 当前屏幕的大小和位置，QtCore.QRect(左上角x坐标,左上角y坐标,x方向宽度，y方向高度)
+        rect_monitor = QtWidgets.QDesktopWidget().availableGeometry()
+        #根据屏幕大小改变窗口大小
+        height_monitor = rect_monitor.height()
+        height_main_win = int(height_monitor*0.9)
+        width_main_win = int(height_main_win*0.9)
+        self.resize(width_main_win,height_main_win)
+        # 当前主窗口的大小和位置，也是QtCore.QRect类型
         qr = self.frameGeometry()
-        cp = QtWidgets.QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
+        #屏幕中心点坐标
+        center_point = rect_monitor.center()#center函数返回当前矩形的中心点位置坐标QtCore.QPoint类型
+        qr.moveCenter(center_point)
         self.move(qr.topLeft())
 
-    def show_about(self):
-        self.about = About_Dialog()
+    def handleEvents(self):
+        self.about_action.triggered.connect(self.showAbout)
+        self.load_data_action.triggered.connect(self.loadData)
+        self.execute_action.triggered.connect(self.execute)
 
-    def load_data(self):
+        self.province_comboBox .activated[str].connect(self.updateProvince)
+        self.target_area_comboBox.activated[str].connect(self.updateTargetArea)
+        self.year_DateEdit .dateChanged.connect(self.updateDatetime)
+        self.density_cell_spinBox.valueChanged.connect(self.updateDensityCell)
+        self.density_class_spinBox .valueChanged.connect(self.updateDensityClass)
+        self.day_cell_spinBox .valueChanged.connect(self.updateDayCell)
+        self.day_class_spinBox.valueChanged.connect(self.updateDayClass)
+
+    def showAbout(self):
+        self.about = AboutDialog()
+
+    def loadData(self):
         fnames = QtWidgets.QFileDialog.getOpenFileNames(self, u'请选择原始的电闪数据',
                                               u'Z:/ZHUF/数据/闪电数据',
                                               'Text files (*.txt);;All(*.*)')
 
-        #self.in_parameters[u'origin_data_path'] = fnames[0]
+        self.in_parameters[u'origin_data_path'] = fnames[0]
 
-class About_Dialog(QtWidgets.QDialog):
+    def updateProvince(self,area):
+        self.in_parameters[u'province'] = area
+        self.target_area_comboBox.clear()
+        if area == u'浙江':
+            self.target_area_comboBox.addItems(['绍兴市','柯桥区','上虞区','诸暨市','嵊州市','新昌县'])
+            self.in_parameters[u'target_area'] = u'绍兴市'
+        elif area == u'河南':
+            self.target_area_comboBox.addItems(['新乡市', '延津县', '新乡县', '辉县市', '卫辉市', '获嘉县','封丘县'])
+            self.in_parameters[u'target_area'] = u'新乡市'
+
+    def updateTargetArea(self,area):
+        self.in_parameters[u'target_area'] = area
+
+    def updateDatetime(self, date):
+        self.in_parameters[u'datetime'] = str(date.year()) + u'年'
+        if self.in_parameters.has_key(u'origin_data_path'):
+            self.in_parameters.__delitem__(u'origin_data_path')
+
+    def updateDensityCell(self, cell):
+        self.in_parameters[u'density_cell'] = str(cell)
+
+    def updateDensityClass(self, nclass):
+        self.in_parameters[u'density_class'] = nclass
+
+    def updateDayCell(self, cell):
+        self.in_parameters[u'day_cell'] = str(cell)
+
+    def updateDayClass(self, nclass):
+        self.in_parameters[u'day_class'] = nclass
+
+    def execute(self):
+
+        dir = u"E:/Documents/工作/雷电公报/闪电定位原始文本数据/" + self.in_parameters[u'datetime']
+
+        if os.path.exists(dir):
+            datafiles = os.listdir(dir)
+            datafiles = map(lambda x:os.path.join(dir,x),datafiles)
+            self.in_parameters[u'origin_data_path'] = datafiles
+
+        if not self.in_parameters.has_key(u'origin_data_path'):
+            message = u"请加载%s的数据" % self.in_parameters[u'datetime']
+            msgBox = QtWidgets.QMessageBox()
+            msgBox.setText(message)
+            msgBox.setIcon(QtWidgets.QMessageBox.Information)
+            icon = QtGui.QIcon()
+            icon.addPixmap(QtGui.QPixmap('./resource/weather-thunder.png'), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            msgBox.setWindowIcon(icon)
+            msgBox.setWindowTitle(" ")
+            msgBox.exec_()
+            return
+
+        self.execute_action.setDisabled(True)
+        self.statusbar.setStatusTip(u'正在制图中……')
+        self.progressBar.setMaximum(0)
+        self.progressBar.setMinimum(0)
+
+        self.load_data_action.setDisabled(True)
+        self.execute_action.setDisabled(True)
+        self.province_comboBox.setDisabled(True)
+        self.target_area_comboBox.setDisabled(True)
+        self.year_DateEdit.setDisabled(True)
+        self.density_cell_spinBox.setDisabled(True)
+        self.density_class_spinBox.setDisabled(True)
+        self.day_cell_spinBox.setDisabled(True)
+        self.day_class_spinBox.setDisabled(True)
+
+        # for outfile in self.in_parameters[u'origin_data_path']:
+        #     infile =
+        #     try:
+        #         with open(infile, 'w+') as in_f:
+        #             for line in in_f:
+        #                 line = line.replace(u"：",":")
+        #                 in_f.write(line)
+        #     except Exception,inst:
+        #         print infile
+
+        self.working_process = WorkProcess()
+        self.working_process.trigger.connect(self.finished)
+        self.working_process.beginRun(self.in_parameters)
+
+    def finished(self):
+        cwd = os.getcwd()
+        #绘制闪电密度图
+        ##清除上一次的QGraphicsView对象，防止其记录上一次图片结果，影响显示效果
+        self.density_view.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.verticalLayout_2.removeWidget(self.density_view)
+        size = self.density_view.size()
+        self.density_view.close()
+
+        self.density_view = QtWidgets.QGraphicsView(self.density_tab)
+        self.density_view.setObjectName("density_view")
+        self.density_view.resize(size)
+        self.verticalLayout_2.addWidget(self.density_view)
+
+        densityPic = ''.join([cwd,u'/temp/',self.in_parameters[u'province'],u'/',
+            self.in_parameters[u'datetime'],u'/',self.in_parameters[u'target_area'], u'.gdb/',
+            self.in_parameters[u'datetime'],self.in_parameters[u'target_area'],u'闪电密度空间分布.tif'])
+
+        scene = QtWidgets.QGraphicsScene()
+        pixmap_density = QtGui.QPixmap(densityPic)
+        scene.addPixmap(pixmap_density)
+        self.density_view.setScene(scene)
+        scale = float(self.density_view.width()) / pixmap_density.width()
+        self.density_view.scale(scale, scale)
+
+        #绘制雷暴日图
+        self.day_view.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.verticalLayout_3.removeWidget(self.day_view)
+        size = self.day_view.size()
+        self.day_view.close()
+
+        self.day_view = QtWidgets.QGraphicsView(self.day_tab)
+        self.day_view.setObjectName("day_view")
+        self.day_view.resize(size)
+        self.verticalLayout_3.addWidget(self.day_view)
+
+        dayPic = ''.join([cwd,u'/temp/',self.in_parameters[u'province'],u'/',
+            self.in_parameters[u'datetime'], u'/',self.in_parameters[u'target_area'], u'.gdb/',
+            self.in_parameters[u'datetime'],self.in_parameters[u'target_area'],u'地闪雷暴日空间分布.tif'])
+
+        pixmap_day = QtGui.QPixmap(dayPic)
+        scene = QtWidgets.QGraphicsScene()
+        scene.addPixmap(pixmap_day)
+        self.day_view.resize(self.density_view.width(),self.density_view.height())
+        self.day_view.setScene(scene)
+        scale = float(self.day_view.width()) / pixmap_day.width()
+        self.day_view.scale(scale, scale)
+
+        #处理进度条和执行按钮状态
+        self.progressBar.setMinimum(0)
+        self.progressBar.setMaximum(100)
+        self.progressBar.setValue(100)
+        self.progressBar.setFormat(u'完成!')
+
+        #改变一些控件的状态
+        self.load_data_action.setDisabled(False)
+        self.execute_action.setDisabled(False)
+        self.province_comboBox.setDisabled(False)
+        self.target_area_comboBox.setDisabled(False)
+        self.year_DateEdit.setDisabled(False)
+        self.density_cell_spinBox.setDisabled(False)
+        self.density_class_spinBox.setDisabled(False)
+        self.day_cell_spinBox.setDisabled(False)
+        self.day_class_spinBox.setDisabled(False)
+
+        #self.action_save_pic.setDisabled(False)
+
+class AboutDialog(QtWidgets.QDialog):
     def __init__(self):
-        super(About_Dialog, self).__init__()
+        super(AboutDialog, self).__init__()
         self.setupUi()
 
     def setupUi(self):
